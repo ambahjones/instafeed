a4fbdb741309a3d6aff66c5f3ae2bc99a0bb345e
ambahjones <67397295+ambahjones@users.noreply.github.com>
2023-01-18 13:53:41 -0500

    instagram feed test app

---
+++ src/components/feed/index.tsx
@@ -0,0 +1,7 @@
+export default function Feed () {
+    return (
+        <div className="grid grid-cols-3 my-12">
+            <img className="h-40" src="" alt="" />
+        </div>
+    )
+}
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,8 @@
+export default function Footer () {
+    return (
+      <div className="flex justify-evenly">
+        <p className="px-2">Like what you see here?</p>
+        <p className="px-2"><a href="/">Follow us!</a></p>
+      </div>
+    );
+}
